ff12be016a6168815e29fd6b0e1fb09ce09a99de
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2022-01-02 16:05:08 +0200

    add local storage functionality to the project

--- Seat-Booking/script.js
+++ Seat-Booking/script.js
@@ -4,14 +4,34 @@ const count = document.getElementById("count")
 const total = document.getElementById("total")
 const movieSelect = document.getElementById("movie");
 
-// using "+" instead of parseInt
-const ticketPrice = +movieSelect.value;
+populateUI();
 
+// using "+" instead of parseInt
+let ticketPrice = +movieSelect.value;
+
+
+// save selected movie index and price
+
+function setMovieData(movieIndex, moviePrice){
+    localStorage.setItem("selectedMovieIndex", movieIndex)
+    localStorage.setItem("selectedMoviePrice", moviePrice)
+}
 //update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected")
+
+    //copy selected seats into arr
+    //map through array
+    //return a new array of indexes
+
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+   localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     
+
+
     count.innerText =  selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
 }
@@ -25,8 +45,31 @@ container.addEventListener("click", e => {
     updateSelectedCount();
 })
 
+//get data from local storage and populate UI
+function populateUI () {
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"))
+    console.log(selectedSeats)
+
+    if(selectedSeats != null && selectedSeats.length > 0) {
+        seats.forEach((seat, index) => {
+            if(selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected")
+            }
+        })
+    }
+
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex")
+    if(selectedMovieIndex != null){
+        movieSelect.selectedIndex = selectedMovieIndex
+    }
+}
+
 // movie select event
 movieSelect.addEventListener("change", e => {
-    ticketPrice = +e.target.value
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value)
     updateSelectedCount()
 })
+
+//initial count and total set 
+updateSelectedCount();
